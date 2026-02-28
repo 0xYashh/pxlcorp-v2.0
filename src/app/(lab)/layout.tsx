1494af53d192a36c827f5be326392e642ddfcc48
Yash Kumar <yashkr044@gmail.com>
2026-02-28 12:43:05 +0530

    fixed rendering issue of bg

--- src/app/(lab)/layout.tsx
+++ src/app/(lab)/layout.tsx
@@ -1,13 +1,13 @@
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 
-export default function FriendsLayout({
+export default function LabLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <div className={`${GeistSans.variable} ${GeistMono.variable} font-sans min-h-screen bg-pxlcorp text-white selection:bg-pxl-red selection:text-white`}>
+        <div className={`${GeistSans.variable} ${GeistMono.variable} font-sans min-h-screen bg-pxlcorp text-white selection:bg-pxl-red selection:text-white relative`}>
             {children}
         </div>
     )
